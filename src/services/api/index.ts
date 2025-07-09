75e56af811b014b9f4fe312839a0e00a0720fa0a
Sardor-M <sardor0968@g.seoultech.ac.kr>
2025-07-09 19:10:51 +0900

    fix: minor importing issue fixed

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -2,6 +2,7 @@ export { useToursFetchService } from '@/services/api/useToursFetchService';
 export { useUserFetchService } from '@/services/api/useUserFetchService';
 export { useUserAuthService } from '@/services/api/useUserAuthService';
 export { useWishlistFetchService } from '@/services/api/useWishlistFetchService';
+export { useBookingFetchService } from '@/services/api/useCheckoutService';
 
 export type { TourListResponse, ToursListApiResponse, TourDetailsResponse } from '@/atoms/tours';
 export type { UserProfile, UpdateUserData, ChangePasswordData } from '@/services/api/useUserFetchService';
