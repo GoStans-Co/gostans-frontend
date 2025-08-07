9b93094cbe3d6c7ebc4fa6a6a11f2ce633e1b0f9
Sardor-M <sardor0968@g.seoultech.ac.kr>
2025-08-07 16:41:29 +0900

    fix: path alias config to vite for @/* added

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
-// https://vite.dev/config/
+import path from 'path';
 
 export default defineConfig({
     plugins: [react()],
     resolve: {
         alias: {
-            '@': '/src',
+            '@': path.resolve(__dirname, 'src'),
         },
     },
 });
